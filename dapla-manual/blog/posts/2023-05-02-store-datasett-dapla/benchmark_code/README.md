18186c644a6684944435d4db27ba8a4b2ccc87cd
Michael Moen Allport <mmallport@gmail.com>
2024-05-10 15:13:47 +0200

    Inital commit

---
+++ dapla-manual/blog/posts/2023-05-02-store-datasett-dapla/benchmark_code/README.md
@@ -0,0 +1,10 @@
+This is a collection of test files used for benchmarking
+
+In order to use, clone the repo in a Jupyter-environment and:
+```bash
+cd dapla-manual-internal
+cd dapla-manual/blog/posts/2023-05-02-store-datasett-dapla/benchmark_code
+poetry install
+poetry shell
+python src/benchmark_code/<filename>
+```
---
+++ dapla-manual/blog/posts/2023-05-02-store-datasett-dapla/benchmark_code/src/benchmark_code/input_read.py
@@ -0,0 +1,21 @@
+import dapla as dp
+import pandas as pd
+from dapla.files import FileClient
+
+from benchmark_code.timer import timing
+
+bucket = "gs://ssb-prod-dapla-felles-data-delt"
+folder = "tech-coach/parquet-test/concat/dataset-1000"
+fs = FileClient().get_gcs_file_system()
+files: list[str] = fs.glob(f"{bucket}/{folder}/*.parquet") # liste av 1000 filer
+
+@timing
+def concat_files_slow() -> pd.DataFrame:
+    dfs = []
+    for file in files:
+        df = dp.read_pandas(gcs_path=file, file_format="parquet")
+        dfs.append(df)
+    df_all = pd.concat(dfs)
+    return df_all
+
+print(concat_files_slow())
