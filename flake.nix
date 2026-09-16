319f494450e56b770fadc83a31af389e93f966b5
skykanin <3789764+skykanin@users.noreply.github.com>
2026-09-16 15:17:29 +0200

    fixup quarto nix package

--- flake.nix
+++ flake.nix
@@ -4,16 +4,19 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
+    nixpkgs-quarto.url = "github:NixOS/nixpkgs/20075955deac2583bb12f07151c2df830ef346b4";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { config, inputs', pkgs, ... }: {
         devShells.default = pkgs.mkShell {
           name = "Dapla manual devShell";
           packages = with pkgs; [
-            quarto
+            # quarto from latest nixpkgs-unstable is broken
+            # quarto
+            inputs'.nixpkgs-quarto.legacyPackages.quarto
           ];
         };
       };
